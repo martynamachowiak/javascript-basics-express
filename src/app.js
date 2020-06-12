992bd0d7292a35252413d7acdbe5cd6a4d2886ca
Martyna Machowiak <martyna.machowiak@gear4music.com>
2020-06-12 22:54:14 +0100

    Add solutions for all string test

--- src/app.js
+++ src/app.js
@@ -1,7 +1,21 @@
 const express = require('express');
+const { sayHello, uppercase, lowercase, firstCharacters } = require('./lib/strings');
 
 const app = express();
 app.get('/strings/hello/:string', (req, res) => {
-  res.status(200).json({ result: `Hello, ${req.params.string}!` });
+  res.status(200).json({ result: sayHello(req.params.string) });
 });
+
+app.get('/strings/upper/:string', (req, res) => {
+  res.status(200).json({ result: uppercase(req.params.string) });
+});
+
+app.get('/strings/lower/:string', (req, res) => {
+  res.status(200).json({ result: lowercase(req.params.string) });
+});
+
+app.get('/strings/first-characters/:string', (req, res) => {
+  res.status(200).json({ result: firstCharacters(req.params.string, req.query.length) });
+});
+
 module.exports = app;
--- src/lib/strings.js
+++ src/lib/strings.js
@@ -1,14 +1,14 @@
-const sayHello = (string) => `Hello, ${string}!`;
+const sayHello = string => `Hello, ${string}!`;
 
-const uppercase = (string) => string.toUpperCase();
+const uppercase = string => string.toUpperCase();
 
-const lowercase = (string) => string.toLowerCase();
+const lowercase = string => string.toLowerCase();
 
-const countCharacters = (string) => string.length;
+const countCharacters = string => string.length;
 
-const firstCharacter = (string) => string[0];
+const firstCharacter = string => string[0];
 
-const firstCharacters = (string, n) => string.slice(0, n);
+const firstCharacters = (string, n = 1) => string.slice(0, n);
 
 module.exports = {
   sayHello,
