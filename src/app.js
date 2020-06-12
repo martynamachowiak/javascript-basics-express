7222b455df39ce3679f493c4f3e7bcafb9e3923f
Martyna Machowiak <martyna.machowiak@gear4music.com>
2020-06-12 21:22:37 +0100

    Add solution for String test

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
 const app = express();
-
+app.get('/strings/hello/world', (req, res) => {
+  res.status(200).json({ result: 'Hello, world!' });
+});
 module.exports = app;
